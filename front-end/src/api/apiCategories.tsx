76057bf8ac34187adf277b30b5c44a3a020c21ed
Priyank Solanki <psolanki@condorcet93.fr>
2024-12-02 22:21:53 +0100

    Update apiCategories.tsx

--- front-end/src/api/apiCategories.tsx
+++ front-end/src/api/apiCategories.tsx
@@ -6,6 +6,7 @@ interface categorie{
     nom:string;
 }
 
+//GET
 export const fetchCategories = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/categories`);
@@ -16,7 +17,7 @@ export const fetchCategories = async () => {
   }
 };
 
-
+//POST
 export const addCategories = async (newCatgories: categorie) => {
     try {
       const response = await axios.post(`${API_BASE_URL}/categories`, newCatgories);
@@ -26,3 +27,26 @@ export const addCategories = async (newCatgories: categorie) => {
       throw error;
     }
   };
+
+
+//PUT
+export const updateCategories = async (id : number, updateCatgories: categorie) => {
+  try {
+    const response = await axios.put(`${API_BASE_URL}/categories/${id}`, updateCatgories);
+    return response.data; 
+  } catch (error) {
+    console.error("Erreur lors de la récupération des produits :", error);
+    throw error;
+  }
+};
+
+//DELETE
+export const deleteCategories = async (id : number) => {
+  try {
+    const response = await axios.delete(`${API_BASE_URL}/categories/${id}`);
+    return response.data; 
+  } catch (error) {
+    console.error("Erreur lors de la récupération des produits :", error);
+    throw error;
+  }
+};
